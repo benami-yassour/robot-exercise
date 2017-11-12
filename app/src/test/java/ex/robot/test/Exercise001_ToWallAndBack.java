062b036ef9b54233972ef032a6c12df108958a94
benfol <benami.yassour@gmail.com>
2017-11-12 21:12:16 +0200

    cl1
    aaaa

--- app/src/test/java/ex/robot/test/Exercise001_ToWallAndBack.java
+++ app/src/test/java/ex/robot/test/Exercise001_ToWallAndBack.java
@@ -6,6 +6,9 @@ import static org.junit.Assert.assertEquals;
 
 /**
  *  Setup:
+ *
+ *
+ *  
  *     Robot location: 0
  *     Balls: None
  *  Task: the robot should go to the right wall (10 steps) and back to location 0.
